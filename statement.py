8c993117884f04bb58d772c839a9510800216ccf
Gustavo de Pinho <gustavo.pinhorezende@gmail.com>
2026-04-20 19:01:13 -0300

    move function - amount_for

--- statement.py
+++ statement.py
@@ -11,10 +11,29 @@ with open('invoices.json', 'r') as file:
 def statement(invoice, plays):
   def play_for(a_performance):
     return plays[a_performance["playID"]]
+  
+  def amount_for(a_performance):
+    result = 0
+    if a_performance["play"]["type"] == "tragedy":
+      result = 40000
+      if a_performance['audience'] > 30:
+        result += 1000 * (a_performance['audience'] - 30)
+    elif a_performance["play"]["type"] == "comedy":
+      result = 30000
+      if a_performance['audience'] > 20:
+        result += 10000 + 500 * (a_performance['audience'] - 20)
+
+      result += 300 * a_performance['audience']
+
+    else:
+      raise ValueError(f'unknown type: {a_performance["play"]["type"]}')
+    
+    return result
 
   def enrich_performance(a_performance):
     result = dict(a_performance)
     result['play'] = play_for(result)
+    result['amount'] = amount_for(result)
     return result
 
   statement_data = {}
@@ -30,7 +49,7 @@ def render_plain_text(data, plays):
   def total_amount():
     result = 0
     for perf in data['performances']:
-      result += amount_for(perf)
+      result += perf['amount']
     return result
 
   def total_volume_credits():
@@ -50,30 +69,11 @@ def render_plain_text(data, plays):
       result += math.floor(a_performance['audience'] / 5)
 
     return result
-
-  def amount_for(a_performance):
-    result = 0
-    if a_performance["play"]["type"] == "tragedy":
-      result = 40000
-      if a_performance['audience'] > 30:
-        result += 1000 * (a_performance['audience'] - 30)
-    elif a_performance["play"]["type"] == "comedy":
-      result = 30000
-      if a_performance['audience'] > 20:
-        result += 10000 + 500 * (a_performance['audience'] - 20)
-
-      result += 300 * a_performance['audience']
-
-    else:
-      raise ValueError(f'unknown type: {a_performance["play"]["type"]}')
-    
-    return result
-
   
   result = f'Statement for {data["customer"]}\n'
 
   for perf in data['performances']:
-    result += f' {perf["play"]["name"]}: {format(amount_for(perf)/100)} ({perf["audience"]} seats)\n'
+    result += f' {perf["play"]["name"]}: {format(perf['amount'])} ({perf["audience"]} seats)\n'
 
   result += f'Amount owed is {format(total_amount())}\n'
   result += f'You earned {total_volume_credits()} credits\n'
